5620862957d0ef4ea48ddaf680a36f9a0d34af60
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-07 18:41:27 +0000

    tox. initial pytest.

---
+++ pytest/test_zetup.py
@@ -0,0 +1,22 @@
+from pkg_resources import get_distribution
+
+from path import path as Path
+
+import zetup
+
+
+def test_zetup():
+    from zetup.zetup import NAME, DISTRIBUTION, VERSION, REQUIRES
+
+    path = Path(zetup.__path__[0])
+
+    assert(NAME == 'zetup')
+
+    assert(zetup.__distribution__ == DISTRIBUTION.find(zetup.__path__[0])
+           == get_distribution('zetup'))
+
+    assert(zetup.__version__ == VERSION
+           == (path / 'VERSION').text().strip())
+
+    assert(str(zetup.__requires__) == str(REQUIRES)
+           == (path / 'requirements.txt').text())
