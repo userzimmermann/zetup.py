5fcfc2c634e147c27d7bf0d1984dfc670e4c25c3
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-07 18:39:34 +0000

    zetup package (__init__ in repo root)

---
+++ __init__.py
@@ -0,0 +1,34 @@
+COMMANDS = ['init']
+
+__all__ = ['COMMANDS'] + COMMANDS
+
+from . import zetup
+
+__distribution__ = zetup.DISTRIBUTION.find(__path__[0])
+__description__ = zetup.DESCRIPTION
+
+__version__ = zetup.VERSION
+__requires__ = zetup.REQUIRES.checked
+
+__notebook__ = zetup.NOTEBOOK
+
+import os
+
+from path import path as Path
+
+
+def init(name, path=None):
+    path = Path(path or os.getcwd())
+    Path(__path__[0] / 'zetup.py').copy(path / '__init__.py')
+    with open(path / 'zetuprc', 'w') as f:
+        f.write("[%s]\n\n%s\n" % (name, "\n".join("%s =" % key for key in [
+          'description',
+          'author',
+          'url',
+          'license',
+          'python',
+          'classifiers',
+          'keywords',
+          ])))
+    with open(path / 'VERSION', 'w') as f:
+        f.write("0.0.0\n")
