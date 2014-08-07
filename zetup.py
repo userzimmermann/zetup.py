968291544930065e4694bf888e734046cf168dfe
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-07 18:35:04 +0000

    Cleanup. Fixes. More doc.

--- zetup.py
+++ zetup.py
@@ -21,6 +21,7 @@ import sys
 import os
 import re
 from collections import OrderedDict
+from subprocess import call
 from pkg_resources import (
   get_distribution, parse_version, parse_requirements,
   DistributionNotFound, VersionConflict)
@@ -33,9 +34,10 @@ else:
     from ConfigParser import ConfigParser
 
 try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+    from setuptools import setup, Command
+except ImportError: # fallback
+    # (setuptools should at least be available after package installation)
+    from distutils.core import setup, Command
 
 
 # Try to get the directory of this script,
@@ -49,7 +51,22 @@ else:
 
 
 class Distribution(str):
+    """Simple proxy to get a pkg_resources.Distribution instance
+       matching the given name and :class:`Version` instance.
+    """
+    def __new__(cls, name, version):
+        return str.__new__(cls, name)
+
+    def __init__(self, name, version):
+        self.version = version
+
     def find(self, modpath, raise_=True):
+        """Try to find the distribution and check version.
+
+        :param raise_: Raise a VersionConflict
+          if version doesn't match the given one?
+          If false just return None.
+        """
         try:
             dist = get_distribution(self)
         except DistributionNotFound:
@@ -57,12 +74,12 @@ class Distribution(str):
         if os.path.realpath(dist.location) \
           != os.path.realpath(os.path.dirname(modpath)):
             return None
-        if dist.parsed_version != VERSION.parsed:
+        if dist.parsed_version != self.version.parsed:
             if raise_:
                 raise VersionConflict(
                   "Version of distribution %s"
                   " doesn't match %s.__version__ %s."
-                  % (dist, PACKAGE, VERSION))
+                  % (dist, PACKAGE, self.version))
             return None
         return dist
 
@@ -73,7 +90,7 @@ class Version(str):
     """
     @staticmethod
     def _parsed(value):
-        """Parse a version `value` if needed.
+        """Parse a version `value` if needed (if simple string).
         """
         if isinstance(value, (str, unicode)):
             value = parse_version(value)
@@ -85,6 +102,8 @@ class Version(str):
         """
         return parse_version(self)
 
+    # Need to override all the compare methods
+    #  (would otherwise be taken from str base)...
     def __eq__(self, other):
         return self.parsed == self._parsed(other)
 
@@ -136,6 +155,13 @@ class Requirements(str):
         self._list = list(reqs())
 
     def check(self, raise_=True):
+        """Check that all requirements are available (importable)
+           and their versions match (using pkg.__version__).
+
+        :param raise_: Raise DistributionNotFound if ImportError
+          or VersionConflict if version doesn't match?
+          If false just return None.
+        """
         for req in self:
             try:
                 mod = __import__(req.modname)
@@ -143,10 +169,14 @@ class Requirements(str):
                 if raise_:
                     raise DistributionNotFound(str(req))
                 return False
-            if mod.__version__ not in req:
+            try:
+                version = mod.__version__
+            except AttributeError as e:
+                raise AttributeError("%s: %s" % (e, mod))
+            if version not in req:
                 if raise_:
                     raise VersionConflict(
-                      "Need %s. Found %s" % (str(req), mod.__version__))
+                      "Need %s. Found %s" % (str(req), version))
                 return False
         return True
 
@@ -159,7 +189,7 @@ class Requirements(str):
         return iter(self._list)
 
     def __getitem__(self, name):
-        """Get a requirement by its package name.
+        """Get a requirement by its distribution name.
         """
         for req in self._list:
             if name in [req.key, req.unsafe_name]:
@@ -182,6 +212,8 @@ class Requirements(str):
 
 class Extras(OrderedDict):
     """Package extra features/requirements manager.
+
+    - Stores :class:`Requirements` instances by extra feature name keys.
     """
     def __setitem__(self, name, text):
         reqs = Requirements(text)
@@ -193,19 +225,22 @@ class Extras(OrderedDict):
           for key, reqs in self.items()))
 
 
+# Read the zetup config...
 config = ConfigParser()
 for fname in ['zetup.ini', 'zetup.cfg', 'zetuprc']:
     if config.read(os.path.join(ZETUP_DIR, fname)):
-        ##TODO: No print after installation (under pkg/zetup/):
+        ##TODO: No print if run from installed package (under pkg/zetup/):
         ## print("zetup: Using config from %s" % fname)
+
+        # The config file will be installed as pkg.zetup package_data:
         ZETUP_DATA = [fname]
         break
 else:
     raise RuntimeError("No zetup config found.")
 
 
+#... and store all setup options in UPPERCASE vars...
 NAME = config.sections()[0]
-DISTRIBUTION = Distribution(NAME)
 
 config = dict(config.items(NAME))
 
@@ -233,13 +268,16 @@ if any(pyversion.startswith('3') for pyversion in PYTHON):
     KEYWORDS.append('python3')
 
 
+# Parse VERSION and requirements files
+#  and add them to pkg.zetup package_data...
 ZETUP_DATA += ['VERSION', 'requirements.txt']
 
 VERSION = Version(open(os.path.join(ZETUP_DIR, 'VERSION')).read().strip())
+DISTRIBUTION = Distribution(NAME, VERSION)
 
 REQUIRES = Requirements(open(os.path.join(ZETUP_DIR, 'requirements.txt')).read())
 
-# Extra requirements to use with setup's extras_require=
+# Look for optional extra requirements to use with setup's extras_require=
 EXTRAS = Extras()
 _re = re.compile(r'^requirements\.(?P<name>[^\.]+)\.txt$')
 for fname in sorted(os.listdir(ZETUP_DIR)):
@@ -249,6 +287,7 @@ for fname in sorted(os.listdir(ZETUP_DIR)):
 
         EXTRAS[match.group('name')] = open(os.path.join(ZETUP_DIR, fname)).read()
 
+# Is there a README notebook?
 NOTEBOOK = os.path.join(ZETUP_DIR, 'README.ipynb')
 if os.path.exists(NOTEBOOK):
     ZETUP_DATA.append('README.ipynb')
@@ -257,19 +296,24 @@ else:
 
 
 def zetup(**setup_options):
-    """Run setup() with options from zetup.cfg
-       and explicit override `setup_options`.
+    """Run setup() with options from zetup config
+       and custom override `setup_options`.
+
+    - Also adds additional setup commands:
+      - ``conda``:
+        conda package builder (with build config generator)
     """
     for option, value in [
       ('name', NAME),
-      ('version', VERSION),
+      ('version', str(VERSION)),
       ('description', DESCRIPTION),
       ('author', AUTHOR),
       ('author_email', EMAIL),
       ('url', URL),
       ('license', LICENSE),
-      ('install_requires', REQUIRES),
-      ('extras_require', EXTRAS),
+      ('install_requires', str(REQUIRES)),
+      ('extras_require',
+       {name: str(reqs) for name, reqs in EXTRAS.items()}),
       ('classifiers', CLASSIFIERS),
       ('keywords', KEYWORDS),
       ]:
