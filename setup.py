dc2527f4ec83c7dc626b8699785ee41f8ef2c555
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-26 15:52:36 +0000

    setup: entry_points, get_version_from_scm

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from zetup import Zetup
 
 zetup = Zetup()
 zetup(
-  # package_dir={'zetup': '.'},
-  # packages=['zetup'],
-  # package_data={'zetup': zetup.ZETUP_DATA},
+  setup_requires=['hgdistver'],
+
+  get_version_from_scm=True,
+
+  entry_points={'distutils.setup_keywords': [
+    'use_zetup = zetup:setup_entry_point',
+    ]},
 
   scripts=['scripts/zetup'],
   )
