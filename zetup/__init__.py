d459e207a607048b5c65db1f57c150626938a3ba
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-26 15:52:03 +0000

    setup_entry_point()

--- zetup/__init__.py
+++ zetup/__init__.py
@@ -17,27 +17,47 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with zetup.py. If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ['Zetup', 'COMMANDS']
+__all__ = ['Zetup']
 
-# Import zetup script to get zetup's own setup data:
-# from . import zetup
-
-
-# z = zetup.Zetup()
-
-# ## __distribution__ = zetup.DISTRIBUTION.find(__path__[0])
-# __description__ = z.DESCRIPTION
-
-# __version__ = z.VERSION
-
-# __requires__ = z.REQUIRES.checked
-# __extras__ = z.EXTRAS
-
-# __notebook__ = z.NOTEBOOKS['README']
-
+import os
 
 from .zetup import Zetup
-from . import commands
+
+# Get zetup's own config:
+try: # from installed zetup package
+    from . import zetup_config
+except ImportError: # from source
+    zetup_config = Zetup(os.path.dirname(os.path.realpath(__path__[0])))
 
 
-COMMANDS = commands.__all__
+__distribution__ = zetup_config.DISTRIBUTION.find(__path__[0])
+__description__ = zetup_config.DESCRIPTION
+
+__version__ = zetup_config.VERSION
+
+__requires__ = zetup_config.REQUIRES # .checked
+__extras__ = zetup_config.EXTRAS
+
+__notebook__ = zetup_config.NOTEBOOKS['README']
+
+
+def setup_entry_point(dist, keyword, value):
+    """zetup's `entry_point` handler for `setup()` in _setup.py_,
+       setting all setup keyword parameters based on zetup config.
+
+    - Activated with `use_setup=True`
+    """
+    assert keyword == 'use_zetup'
+    if not value:
+        return
+
+    zetup = Zetup() # reads config from current working directory
+                    #  (where setup.py is run)
+    keywords = zetup.setup_keywords()
+    for name, value in keywords.items():
+        # Generally, setting stuff on dist.metadata is enough
+        #  (and necessary), but *pip* only works correct
+        #  if stuff is also set directly on dist object
+        #  (seems to read at least packages list somehow from there):
+        for obj in [dist, dist.metadata]:
+            setattr(obj, name, value)
