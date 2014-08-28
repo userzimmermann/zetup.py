62f3f398ce530b3967bd589caeb0d8794b541c6f
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-28 21:46:05 +0000

    fixed/extended test_zetup

--- pytest/test_zetup.py
+++ pytest/test_zetup.py
@@ -1,22 +1,31 @@
+from inspect import ismodule
+
 from pkg_resources import get_distribution
 
 from path import path as Path
 
 import zetup
+from zetup import Zetup, find_zetup_config
 
 
 def test_zetup():
-    from zetup import zetup as z
+    zfg = find_zetup_config('zetup')
 
-    path = Path(z.ZETUP_DIR)
+    pkg_path = Path(zetup.__path__[0]).realpath()
+    zfg_path = Path(zfg.ZETUP_DIR).realpath()
 
-    assert(z.NAME == 'zetup')
+    assert ismodule(zfg) and zfg_path == (pkg_path / 'zetup_config') \
+      or isinstance(zfg, Zetup) and zfg_path == pkg_path.dirname()
 
-    ## assert(zetup.__distribution__ == z.DISTRIBUTION.find(zetup.__path__[0])
-    ##        == get_distribution('zetup'))
+    assert(zfg.NAME == 'zetup')
 
-    assert(zetup.__version__ == z.VERSION
-           == (path / 'VERSION').text().strip())
+    assert(zetup.__distribution__ == zfg.DISTRIBUTION.find(pkg_path)
+           == get_distribution('zetup'))
 
-    ## assert(str(zetup.__requires__) == str(z.REQUIRES)
-    ##        == (path / 'requirements.txt').text())
+    assert(zetup.__description__ == zfg.DESCRIPTION)
+
+    assert(zetup.__version__ == zfg.VERSION
+           == (zfg_path / 'VERSION').text().strip())
+
+    assert(str(zetup.__requires__) == str(zfg.REQUIRES))
+           ## == (path / 'requirements.txt').text())
