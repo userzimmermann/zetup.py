0d2cde9f28decaf93cb0e41d8c72f765f2025af7
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-09-01 20:28:55 +0000

    commands.run

---
+++ zetup/commands/run.py
@@ -0,0 +1,29 @@
+# zetup.py
+#
+# Zimmermann's Python package setup.
+#
+# Copyright (C) 2014 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# zetup.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# zetup.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with zetup.py. If not, see <http://www.gnu.org/licenses/>.
+
+from subprocess import call
+
+from zetup import Zetup
+
+
+@Zetup.command(depends=['all'])
+def run(self, args=None, cmd=None):
+    if args:
+        cmd = args.cmd
+    return call(cmd)
