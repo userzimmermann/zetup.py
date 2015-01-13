c334112276860504ad062b0123307130aeb567bd
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-01-13 23:21:21 +0000

    setup: commented out hgdistver usage via setup_requires (problematic revision hashes in .dev versions)

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup['package_data']['zetup.commands.make'] = [
   'templates/package/*.jinja',
   ]
 setup(
-  setup_requires=['hgdistver >= 0.23'],
+  ## setup_requires=['hgdistver >= 0.23'],
 
-  get_version_from_scm=True,
+  ## get_version_from_scm=True,
 
   entry_points={'distutils.setup_keywords': [
     'use_zetup = zetup:setup_entry_point',
