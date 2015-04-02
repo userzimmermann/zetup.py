1cae4abebaf8ad537b3927564b09fd3cdb42c1ee
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-04-02 06:54:51 +0000

    conftest: in_site_packages/in_repo fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,8 @@
 
 .. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
 """
+from inspect import ismodule
+
 from zetup import find_zetup_config
 
 import pytest
@@ -12,3 +14,17 @@ def zfg():
     """Get zetup's own config.
     """
     return find_zetup_config('zetup')
+
+
+@pytest.fixture
+def in_site_packages():
+    """Is zetup imported from repository?
+    """
+    return ismodule(zfg())
+
+
+@pytest.fixture
+def in_repo():
+    """Is zetup imported from site-packages?
+    """
+    return not in_site_packages()
