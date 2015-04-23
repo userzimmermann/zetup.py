11b5d787b347cebf30da4b313a3570f6870e4e80
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-04-23 22:48:55 +0200

    setup: try import zetup.commands again + (commented out) setuptools dependency resolving

--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
-from textwrap import dedent
-from warnings import warn
+## import sys
 
-from zetup import Zetup
+## from setuptools import Distribution
+## from pkg_resources import get_distribution
+
+from zetup import Zetup, DistributionNotFound, VersionConflict
+try:
+    import zetup.commands
+except (ImportError, DistributionNotFound, VersionConflict):
+    pass
 
 
-zetup = Zetup()
+## setup_req = 'setuptools >= 15.0'
+## try:
+##     get_distribution(setup_req)
+## except VersionConflict:
+##     for mod in ['setuptools', 'pkg_resources']:
+##         for name, _ in list(sys.modules.items()):
+##             if name == mod or name.startswith(mod + '.'):
+##                 del sys.modules[name]
+##     sys.path.insert(0, Distribution().fetch_build_egg(setup_req))
 
-setup = zetup.setup
+
+zfg = Zetup()
+
+setup = zfg.setup
 setup['package_data']['zetup.commands.make'] = [
   'templates/*.jinja',
   'templates/package/*.jinja',
   ]
 setup(
+  ## setup_requires=setup_req,
   ## setup_requires=['hgdistver >= 0.23'],
 
   ## get_version_from_scm=True,
