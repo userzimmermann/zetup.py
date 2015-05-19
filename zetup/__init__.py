f4d5892c504a1e9e9c4eff12a0b9ad906ab62e03
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-05-19 19:18:31 +0200

    ZetupModule wrapper

--- zetup/__init__.py
+++ zetup/__init__.py
@@ -23,12 +23,42 @@ __all__ = ['find_zetup_config', 'Zetup',
 
 import sys
 import os
+from types import ModuleType
 ## from inspect import ismodule
 
 from .zetup import Zetup
 from .error import ZetupError
 from .config import ZetupConfigNotFound
 from .requires import DistributionNotFound, VersionConflict
+
+
+class ZetupModule(ModuleType):
+    """Custom module class for wrapping native `zetup` module on import.
+    """
+    def __init__(self):
+        self.__name__ = __name__
+        self.module = sys.modules[__name__]
+
+    def __getattr__(self, name):
+        """Just delegate to native `zetup` module.
+        """
+        return getattr(self.module, name)
+
+    def __dir__(self):
+        """Just delegate to native `zetup` module.
+        """
+        return dir(self.module)
+
+    @property
+    def Path(self):
+        """Provide **path.py**-wrapping `zetup.Path` class
+           if **path.py** is installed.
+        """
+        from .path import Path
+        return Path
+
+# replace native zetup module with wrapper
+sys.modules[__name__] = ZetupModule()
 
 
 COMMANDS = {}
