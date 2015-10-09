d4d044ad8777afa2db536f983c1e70ab97f538d7
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-10-09 18:13:50 +0200

    Distribution: backwards compatible constructor + auto-reinstall zetup's own project + some cleanup

--- zetup/dist.py
+++ zetup/dist.py
@@ -20,6 +20,8 @@
 __all__ = ['Distribution']
 
 import os
+
+import pkg_resources
 from pkg_resources import (
   get_distribution, DistributionNotFound, VersionConflict)
 
@@ -28,32 +30,49 @@ from .version import Version
 
 class Distribution(str):
     """Simple proxy to get a pkg_resources.Distribution instance
-       matching the given name and :class:`Version` instance.
+       matching dist name and version from a zetup config object.
     """
-    def __new__(cls, zfg):
-        return str.__new__(cls, zfg.NAME)
+    def __new__(cls, zfg_or_name, mainpkg=None, version=None):
+        if isinstance(zfg_or_name, str):
+            name = zfg_or_name
+        else:
+            name = zfg_or_name.NAME
+        return str.__new__(cls, name)
 
-    def __init__(self, zfg):
-        """Initialize with distribution `name`, top level `pkg` name
-           and `version` string.
+    def __init__(self, zfg_or_name, mainpkg=None, version=None):
+        """Initialize with zetup config in `zfg_or_name`.
+
+        - Supports dist name as first argument
+          and remaining arguments for backwards compatibility.
         """
-        self.zfg = zfg
+        if not isinstance(zfg_or_name, str):
+            self.zfg = zfg_or_name
+        else:
+            self.zfg = None
+            self.__dict__['version'] = version and Version(version)
 
     @property
     def version(self):
         """Get version from zetup config as :class:`zetup.Version` instance.
         """
-        return self.zfg.VERSION and Version(self.zfg.VERSION)
+        # first look for explicit version from __init__
+        # for backwards compatibility
+        return self.__dict__.get('version') \
+            or self.zfg.VERSION and Version(self.zfg.VERSION)
 
     def find(self, modpath, raise_=True):
         """Try to find the distribution and check version.
 
         - Also checks if distribution is in the same directory
           as given `modpath`.
+        - Automatically reinstalls zetup in develop mode
+          on distribution version mismatch
+          if handling zetup's own config from zetup's project src.
 
-        :param raise_: Raise a VersionConflict
-          if version doesn't match the given one?
-          If false just return None.
+        :param raise_:
+           Raise a ``VersionConflict``
+           if version doesn't match the given one?
+           If ``False`` just return ``None``.
         """
         # If no version is given (for whatever reason), just do nothing:
         if not self.version:
@@ -67,12 +86,29 @@ class Distribution(str):
           != os.path.normcase(os.path.dirname(os.path.realpath(modpath))):
             return None
         if dist.parsed_version != self.version.parsed:
-            if raise_:
+            message = (
+                "Version of distribution %s "
+                "doesn't match version %s from %s. "
+                % (repr(dist), self.version, repr(self.zfg)))
+            # are we handling zetup's own config?
+            if self == 'zetup':
+                from zetup.zetup import Zetup
+                # and was it loaded from zetup's project src?
+                if isinstance(self.zfg, Zetup):
+                    print('zetup:', message)
+                    # then automatically reinstall zetup in develop mode
+                    print('zetup: Reinstalling in develop mode...')
+                    import zetup.commands.dev
+                    self.zfg.dev()
+                    # reset setuptools distribution cache
+                    pkg_resources.working_set = pkg_resources.WorkingSet(
+                        pkg_resources.working_set.entries)
+                    # and return updated dist
+                    return get_distribution(self)
+
+            elif raise_:
                 raise VersionConflict(
-                    "Version of distribution %s "
-                    "doesn't match version %s from %s. "
-                    % (repr(dist), self.version, self.zfg) +
-                    "Please reinstall %s." % repr(self.zfg.NAME))
+                    message + "Please reinstall %s." % str.__repr__(self))
             return None
         return dist
 
