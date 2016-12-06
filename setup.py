04322917f8f942f53ca9aa2cff4ec8cdbefec7d6
Stefan Zimmermann <zimmermann.code@gmail.com>
2016-12-06 20:50:12 +0100

    setup: Some cleanup + Check setuptools

--- setup.py
+++ setup.py
@@ -3,23 +3,26 @@ from __future__ import print_function
 import sys
 import os
 
-## from setuptools import Distribution
+# from setuptools import Distribution
 from pkg_resources import get_distribution, working_set, VersionConflict
 
 
 def samefile(path, other):
+    """Workaround for missing ``os.path.samefile`` in Windows Python 2.7
+    """
     return os.path.normcase(os.path.normpath(os.path.realpath(path))) \
         == os.path.normcase(os.path.normpath(os.path.realpath(other)))
 
 
-sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 try:
     import zetup
 except VersionConflict:
     egg_info = 'zetup.egg-info'
     dist = get_distribution('zetup')
-    if samefile(dist.location, os.path.dirname(os.path.realpath(__file__))) \
-      and os.path.exists(egg_info):
+    if samefile(
+            dist.location, os.path.dirname(os.path.realpath(__file__))
+    ) and os.path.exists(egg_info):
         print("zetup: Removing possibly outdated %s/" % egg_info,
               # don't pollute stdout
               file=sys.stderr)
@@ -38,23 +41,24 @@ from zetup import Zetup, DistributionNotFound, VersionConflict
 try:
     from zetup.commands import make, pytest, tox, conda
 except (ImportError, DistributionNotFound, VersionConflict):
-    #==> no zetup commands available
+    # ==> no zetup commands available
     # standard setup commands work anyway
     pass
 
 
-## setup_req = 'setuptools >= 15.0'
-## try:
-##     get_distribution(setup_req)
-## except VersionConflict:
-##     for mod in ['setuptools', 'pkg_resources']:
-##         for name, _ in list(sys.modules.items()):
-##             if name == mod or name.startswith(mod + '.'):
-##                 del sys.modules[name]
-##     sys.path.insert(0, Distribution().fetch_build_egg(setup_req))
+# setup_req = 'setuptools >= 15.0'
+# try:
+#     get_distribution(setup_req)
+# except VersionConflict:
+#     for mod in ['setuptools', 'pkg_resources']:
+#         for name, _ in list(sys.modules.items()):
+#             if name == mod or name.startswith(mod + '.'):
+#                 del sys.modules[name]
+#     sys.path.insert(0, Distribution().fetch_build_egg(setup_req))
 
 
 zfg = Zetup()
+zetup.requires.Requirements('setuptools >= 27.0', zfg=zfg).check()
 
 setup = zfg.setup
 setup['package_data']['zetup.commands.make'] = [
