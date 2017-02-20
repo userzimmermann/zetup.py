45af3981cf3158b3650d0cc022f92708b192f91c
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-02-20 07:55:20 +0100

    Add __main__ module

---
+++ zetup/__main__.py
@@ -0,0 +1,29 @@
+# ZETUP
+#
+# Zimmermann's Extensible Tools for Unified Project setups
+#
+# Copyright (C) 2014-2016 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# ZETUP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ZETUP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ZETUP. If not, see <http://www.gnu.org/licenses/>.
+
+"""zetup.__main__
+
+.. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
+"""
+from __future__ import absolute_import
+
+import zetup.script
+
+
+zetup.script.run()
